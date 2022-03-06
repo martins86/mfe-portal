480e849a3f3175f7c9426496a6a4cc0e99afda15
Thiago Martins de Freitas <martins86@gmail.com>
2022-03-06 13:02:50 -0300

    chore: editando o karma.conf.js

--- projects/portal/karma.conf.js
+++ projects/portal/karma.conf.js
@@ -1,41 +1,15 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+import getBaseKarmaConfig from './../../karma.root.conf';
 
-module.exports = function (config) {
+export default function (config) {
+  var baseRootConfig = getBaseKarmaConfig();
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-    ],
-    client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-    jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
-    },
+    ...baseRootConfig,
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/portal'),
+      dir: require('path').join(__dirname, '../../coverage/shared-lib'),
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+      reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'lcov' }],
+      fixWebpackSourcePaths: true,
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true,
-  })
-}
+  });
+};
