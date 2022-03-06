ef9afa8abcfc4c8641bf3ac70a018f5c90039043
Thiago Martins de Freitas <martins86@gmail.com>
2022-03-06 12:49:48 -0300

    chore: adicionado o karma.root.conf.js

---
+++ karma.root.conf.js
@@ -0,0 +1,52 @@
+module.exports = function () {
+  return {
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false,
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    thresholds: {
+      emitWarning: false,
+      global: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80,
+      },
+      each: {
+        statements: 80,
+        lines: 80,
+        branches: 80,
+        functions: 80,
+      },
+    },
+    reporters: ['progress', 'coverage', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--headless', '--no-sandbox', '--remote-debugging-port=9222'],
+      },
+    },
+    browserDisconnectTolerance: 8,
+    browserNoActivityTimeout: 60000,
+    browserDisconnectTimeout: 20000,
+    captureTimeout: 210000,
+  };
+};
