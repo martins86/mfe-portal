52461afcde6d6ba211737a629e0efa75d824d77f
Thiago Martins de Freitas <martins86@gmail.com>
2022-03-12 01:47:31 -0300

    test: corrigindo testes e exclude module

--- projects/portal/src/app/layout/default/default.component.spec.ts
+++ projects/portal/src/app/layout/default/default.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { RouterTestingModule } from '@angular/router/testing'
 
+import { DefaultModule } from './default.module'
 import { DefaultComponent } from './default.component'
 
 describe('DefaultComponent', () => {
@@ -9,6 +11,7 @@ describe('DefaultComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DefaultComponent],
+      imports: [DefaultModule, RouterTestingModule],
     }).compileComponents()
   })
 
