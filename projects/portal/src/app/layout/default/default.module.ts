1e1a889694ea2844973d7ab135f73d875b7e0e78
Thiago Martins de Freitas <martins86@gmail.com>
2022-03-12 00:48:18 -0300

    feat(layout): iniciando components do layout

--- projects/portal/src/app/layout/default/default.module.ts
+++ projects/portal/src/app/layout/default/default.module.ts
@@ -4,8 +4,14 @@ import { CommonModule } from '@angular/common'
 import { DefaultRoutingModule } from './default-routing.module'
 import { DefaultComponent } from './default.component'
 
+import { HeaderComponent } from './header/header.component'
+import { LeftBarComponent } from './left-bar/left-bar.component'
+import { FooterComponent } from './footer/footer.component'
+
+const layoutModules = [HeaderComponent, LeftBarComponent, FooterComponent]
+
 @NgModule({
-  declarations: [DefaultComponent],
+  declarations: [DefaultComponent, ...layoutModules],
   imports: [CommonModule, DefaultRoutingModule],
 })
 export class DefaultModule {}
---
+++ projects/portal/src/app/layout/default/left-bar/left-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-left-bar',
+  templateUrl: './left-bar.component.html',
+  styleUrls: ['./left-bar.component.scss'],
+})
+export class LeftBarComponent {}
