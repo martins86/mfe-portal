452d012b4eb8e1f671bede7f4721b524d9709a16
Thiago Martins de Freitas <martins86@gmail.com>
2022-03-21 16:40:28 -0300

    chore(layout): adicionando prop maintenance

--- projects/portal/src/app/layout/default/default.component.ts
+++ projects/portal/src/app/layout/default/default.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core'
 })
 export class DefaultComponent {
   leftBarOpen = false
+  maintenance = true
 
   toggleLeftBar(): void {
     this.leftBarOpen = !this.leftBarOpen
