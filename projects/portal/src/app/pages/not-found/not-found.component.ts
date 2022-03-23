cfe530277918aae6d59356d1cbbfc7169cc9c483
Thiago Martins de Freitas <martins86@gmail.com>
2022-03-23 16:41:46 -0300

    fix(sonar): resolvendo code smells

--- projects/portal/src/app/pages/not-found/not-found.component.ts
+++ projects/portal/src/app/pages/not-found/not-found.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss'],
+  template: `<div
+    class="container mt-4 mb-4 d-flex align-items-end flex-column justify-content-end"
+    style="height: calc(100% - 48px)"
+  >
+    <div class="row">
+      <div class="col d-flex align-items-end">
+        <p class="text-end" style="color: var(--text-accent-color)">
+          ERRO 404!! - Conteúdo não encontrado.
+        </p>
+      </div>
+    </div>
+  </div>`,
 })
 export class NotFoundComponent {}
