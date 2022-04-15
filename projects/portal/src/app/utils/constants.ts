6965523f108ff20c42e1be3d380d13d3c9226cd6
Thiago Martins de Freitas <martins86@gmail.com>
2022-04-15 14:16:26 -0300

    feat: iniciando util constants

---
+++ projects/portal/src/app/utils/constants.ts
@@ -0,0 +1,6 @@
+export const ConstantsTranslations = {
+  englishLanguage: 'en',
+  englishTranslations: './../../../../assets/i18n/en.json',
+  portugueseLanguage: 'pt',
+  portugueseTranslations: './../../../../assets/i18n/pt.json',
+}
