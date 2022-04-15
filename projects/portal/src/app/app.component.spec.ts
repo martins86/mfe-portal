2f093655a6bd8f794efd9c5ec4e93ef9ad771940
Thiago Martins de Freitas <martins86@gmail.com>
2022-04-15 15:00:45 -0300

    feat(testes): ajustes nos testes

--- projects/portal/src/app/app.component.spec.ts
+++ projects/portal/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
+import { AppModule } from './app.module'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, AppModule],
       declarations: [AppComponent],
     }).compileComponents()
   })
--- projects/portal/src/app/layout/default/default.component.spec.ts
+++ projects/portal/src/app/layout/default/default.component.spec.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { DefaultModule } from './default.module'
 import { DefaultComponent } from './default.component'
+import { TranslateStubModule } from './../../__stubs__/translate-stub.module'
 
 describe('DefaultComponent', () => {
   let component: DefaultComponent
@@ -12,7 +14,13 @@ describe('DefaultComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DefaultComponent],
-      imports: [DefaultModule, BrowserAnimationsModule, RouterTestingModule],
+      imports: [
+        DefaultModule,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        TranslateStubModule,
+      ],
     }).compileComponents()
   })
 
